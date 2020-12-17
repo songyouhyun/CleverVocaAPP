ea4f8cbd7f7e9dbf8683a21134ad967b8b2f6236
lot8229 <lot8229@kakao.com>
2020-12-17 16:18:17 +0900

    update testselect

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -57,10 +57,78 @@ public class Main {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.getContentPane().setLayout(null);
+				JPanel TestPage = new JPanel();
+				TestPage.setBackground(Color.WHITE);
+				TestPage.setBounds(0, 0, 1920, 1080);
+				frame.getContentPane().add(TestPage);
+				TestPage.setLayout(null);
+				
+						
+				
+						JLabel lblTest = new JLabel("Test");
+						lblTest.setHorizontalAlignment(SwingConstants.CENTER);
+						lblTest.setBounds(695, 97, 560, 130);
+						TestPage.add(lblTest);
+						lblTest.setFont(BlackBrutal);
+						TestPage.setVisible(false);
+						lblTest.setFont(BlackBrutal);
+						
+						JButton SelectBtnWrite = new JButton("WRITE");
+						SelectBtnWrite.setBounds(1255, 540, 280, 130);
+						SelectBtnWrite.setBackground(Color.white);
+						SelectBtnWrite.setFont(null);
+						SelectBtnWrite.setBorder(null);
+						try {
+							BlackBrutal = Font.createFont(Font.TRUETYPE_FONT, new File("BlackBrutal.ttf")).deriveFont(80f);
+							GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+							ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("BlackBrutal.ttf")));
+						} catch (IOException | FontFormatException e) {
+
+						}
+						SelectBtnWrite.setFont(BlackBrutal);
+						TestPage.add(SelectBtnWrite);
+						
+						
+						JButton SelectBtnCard = new JButton("CARD");
+						SelectBtnCard.setBounds(415, 540, 280, 130);
+						TestPage.add(SelectBtnCard);
+		
+				JPanel WriteTestPage = new JPanel();
+				WriteTestPage.setBounds(0, 0, 1920, 1080);
+				frame.getContentPane().add(WriteTestPage);
+				WriteTestPage.setLayout(null);
+				WriteTestPage.setVisible(false);
+				
+						JPanel lineborderPanel = new JPanel();
+						lineborderPanel.setBorder(new LineBorder(new Color(0, 0, 0), 20, true));
+						lineborderPanel.setBounds(290, 290, 1370, 700);
+						WriteTestPage.add(lineborderPanel);
+						lineborderPanel.setLayout(null);
+						
+								JLabel WritetestWord = new JLabel("clever");
+								WritetestWord.setHorizontalAlignment(SwingConstants.CENTER);
+								WritetestWord.setBounds(409, 97, 560, 130);
+								lineborderPanel.add(WritetestWord);
+								WritetestWord.setFont(BlackBrutal);
+								
+										txtWriteHere = new JTextField();
+										txtWriteHere.setHorizontalAlignment(SwingConstants.CENTER);
+										txtWriteHere.setBounds(409, 375, 560, 130);
+										lineborderPanel.add(txtWriteHere);
+										txtWriteHere.setColumns(10);
+										txtWriteHere.setFont(BlackBrutal);
+										
+												JLabel Writetestlabel = new JLabel("Write Test");
+												Writetestlabel.setHorizontalAlignment(SwingConstants.CENTER);
+												Writetestlabel.setBounds(695, 97, 560, 130);
+												Writetestlabel.setFont(BlackBrutal);
+												WriteTestPage.add(Writetestlabel);
+												Writetestlabel.setFont(BlackBrutal);
 
 		JPanel StartPage = new JPanel();
 		StartPage.setBackground(Color.WHITE);
 		StartPage.setBounds(0, 0, 1902, 1033);
+		
 		frame.getContentPane().add(StartPage);
 		StartPage.setLayout(null);
 
@@ -121,11 +189,6 @@ public class Main {
 			
 		}
 		btnTest.setFont(BlackBrutal);
-		JPanel TestPage = new JPanel();
-		TestPage.setBackground(Color.WHITE);
-		TestPage.setBounds(0, 0, 1920, 1080);
-		frame.getContentPane().add(TestPage);
-		TestPage.setLayout(null);
 
 		btnTest.addActionListener(new ActionListener() {
 
@@ -135,15 +198,6 @@ public class Main {
 				TestPage.setVisible(true);
 			}
 		});
-
-		
-
-		JLabel lblTest = new JLabel("Test");
-		lblTest.setHorizontalAlignment(SwingConstants.CENTER);
-		lblTest.setBounds(695, 97, 560, 130);
-		TestPage.add(lblTest);
-		lblTest.setFont(BlackBrutal);
-		TestPage.setVisible(false);
 		try {
 			BlackBrutal = Font.createFont(Font.TRUETYPE_FONT, new File("BlackBrutal.ttf")).deriveFont(170f);
 			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
@@ -151,24 +205,6 @@ public class Main {
 		} catch (IOException | FontFormatException e) {
 
 		}
-		lblTest.setFont(BlackBrutal);
-
-		JPanel WriteTestPage = new JPanel();
-		WriteTestPage.setBounds(0, 0, 1920, 1080);
-		frame.getContentPane().add(WriteTestPage);
-		WriteTestPage.setLayout(null);
-		WriteTestPage.setVisible(false);
-
-		JPanel lineborderPanel = new JPanel();
-		lineborderPanel.setBorder(new LineBorder(new Color(0, 0, 0), 20, true));
-		lineborderPanel.setBounds(290, 290, 1370, 700);
-		WriteTestPage.add(lineborderPanel);
-		lineborderPanel.setLayout(null);
-
-		JLabel WritetestWord = new JLabel("clever");
-		WritetestWord.setHorizontalAlignment(SwingConstants.CENTER);
-		WritetestWord.setBounds(409, 97, 560, 130);
-		lineborderPanel.add(WritetestWord);
 		try {
 			BlackBrutal = Font.createFont(Font.TRUETYPE_FONT, new File("BlackBrutal.ttf")).deriveFont(100f);
 			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
@@ -176,13 +212,6 @@ public class Main {
 		} catch (IOException | FontFormatException e) {
 
 		}
-		WritetestWord.setFont(BlackBrutal);
-
-		txtWriteHere = new JTextField();
-		txtWriteHere.setHorizontalAlignment(SwingConstants.CENTER);
-		txtWriteHere.setBounds(409, 375, 560, 130);
-		lineborderPanel.add(txtWriteHere);
-		txtWriteHere.setColumns(10);
 		try {
 			BlackBrutal = Font.createFont(Font.TRUETYPE_FONT, new File("BlackBrutal.ttf")).deriveFont(130f);
 			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
@@ -190,12 +219,6 @@ public class Main {
 		} catch (IOException | FontFormatException e) {
 
 		}
-		txtWriteHere.setFont(BlackBrutal);
-
-		JLabel Writetestlabel = new JLabel("Write Test");
-		Writetestlabel.setHorizontalAlignment(SwingConstants.CENTER);
-		Writetestlabel.setBounds(695, 97, 560, 130);
-		Writetestlabel.setFont(BlackBrutal);
 		try {
 			BlackBrutal = Font.createFont(Font.TRUETYPE_FONT, new File("BlackBrutal.ttf")).deriveFont(130f);
 			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
@@ -203,8 +226,6 @@ public class Main {
 		} catch (IOException | FontFormatException e) {
 
 		}
-		WriteTestPage.add(Writetestlabel);
-		Writetestlabel.setFont(BlackBrutal);
 
 		try {
 			BlackBrutal = Font.createFont(Font.TRUETYPE_FONT, new File("BlackBrutal.ttf")).deriveFont(70f);
